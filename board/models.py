4f73e63d88a17b792b1a5e5f09661accbe97455a
sinthushan <sinthushan@gmail.com>
2023-12-27 14:10:57 -0500

    added basic model structure for mood board application

--- board/models.py
+++ board/models.py
@@ -1,20 +1,27 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from creator.models import Creator
 
-User = get_user_model()
 
+
+class Gallery(models.Model):
+    # an instance of this model should be created when a new creator is created
+    creator = models.OneToOneField(Creator, on_delete=models.CASCADE, related_name="gallery")
 
 class Board(models.Model):
     # we wont be setting a primary key as django will automatically 
     # create an ID field and set it as the primary key
     board_name = models.CharField(max_length=250)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE) 
+    creator = models.ForeignKey(Creator, on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+    board_height_px = models.IntegerField()
+    board_width_px = models.IntegerField() 
     number_of_columns = models.IntegerField()
     number_of_rows = models.IntegerField()
 
 
 class Inspo(models.Model):
-    board_ID =  models.ForeignKey(Board, on_delete=models.CASCADE)
+    gallery_ID =  models.ForeignKey(Gallery, on_delete=models.CASCADE)
+    board_ID =  models.ForeignKey(Board, on_delete=models.CASCADE, blank=True, null=True)
     image_url = models.URLField()
     column_coordinate = models.IntegerField()
     row_coordiantes = models.IntegerField()
