c5a71ea27a501f5a5eec2571b46ffbc171e06ade
sinthushan <sinthushan@gmail.com>
2024-01-20 16:45:59 -0500

    added images from creator's gallery to the create board page

--- board/views.py
+++ board/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+import os
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from creator.models import Creator
 
-@login_required
+
+@login_required(login_url='/creator/login')
 def create_board(request):
-    print(request.User)
-    return render(request, "board/create.html", )
+    user_id = request.user.id
+    creator =  Creator.objects.get(user_id=user_id)
+    gallery = creator.gallery
+    
+    inspos = gallery.inspo_set.all()
+    images = ['../' + inspo.image_url for inspo in inspos]
+
+    return render(request, "board/create.html", {"images": images})
 
 def delete_board(request, id):
     return HttpResponse(f"the following board will be deleted {id}")
